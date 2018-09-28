de4841612303c2cfd4dc250b687d830d9400f902
Erik Aneer <erik.aneer@yh.nackademin.se>
2018-09-28 10:21:43 +0200

    Initial commit (empty project)

---
+++ src/inlamningsuppgift1/Inlamningsuppgift1.java
@@ -0,0 +1,17 @@
+/*
+ * Javautveckling 2018
+ */
+
+package inlamningsuppgift1;
+
+
+public class Inlamningsuppgift1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+
+}
